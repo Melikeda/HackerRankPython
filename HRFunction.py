c872db52c7fe1ff0d3ebbdf8d9bc56cd4aa9417d
Melike Külahcı <m.edakulahci@gmail.com>
2025-10-06 21:19:17 +0300

    HRFunction.py

---
+++ HRFunction.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    def my_function():
+        i = 1
+        while i <= n:
+            print(i, end="")
+            i +=1
+            
+    my_function()        
+    
