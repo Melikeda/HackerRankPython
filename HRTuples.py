149deb455abaa5abe9b67f6a9d49063cb6f4611d
Melike Külahcı <m.edakulahci@gmail.com>
2025-10-07 18:49:04 +0300

    HRTuples.py

---
+++ HRTuples.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    
+    t = tuple(integer_list)
+
+    print(hash(t))  // “Hash”, bir veri yapısını veya değeri sabit uzunlukta bir sayıya dönüştürme işlemidir.
