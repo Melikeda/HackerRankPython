73de8489f4dca02cdbe3f9c1bde83a6c1b7dc628
Melike Külahcı <m.edakulahci@gmail.com>
2025-10-23 19:39:56 +0300

    HRSets.py

---
+++ HRSets.py
@@ -0,0 +1,10 @@
+    
+    x = set(array)             
+    return sum(x) / len(x)
+    
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
