167efa08a441bb6a5ca6aad2defcd0218d924e77
Melike Külahcı <m.edakulahci@gmail.com>
2025-10-30 21:46:56 +0300

    HR_MapandLambdaFunction.py

---
+++ HR_MapandLambdaFunction.py
@@ -0,0 +1,15 @@
+cube = lambda x: x**3
+
+def fibonacci(n):
+    if n == 0:
+        return []
+    elif n == 1:
+        return [0]  
+    fib = [0, 1]  
+    for i in range(2, n):
+        fib.append(fib[-1] + fib[-2])
+    return fib
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
